da02fce3d901c7a2fc197eb4bd260593b9c21885
Andres Bustos <seoamd2@agenciadigitalamd.com>
2026-03-26 17:44:37 -0500

    Version inicial y funcional de automatizacion de prominencia, el flujo llega e indexa la palabra clave y la url que se quiere modificar

---
+++ youtube.py
@@ -0,0 +1,69 @@
+import sys
+
+def go_to_youtube(page, browser):
+    page.goto("https://www.youtube.com/")
+    page.wait_for_timeout(5000)
+    page.wait_for_selector("input[name='search_query']")
+    page.fill("input[name='search_query']", "Dolar hoy en colombia")
+    page.press("input[name='search_query']", "Enter")
+    page.wait_for_timeout(5000)
+
+    # Click en el primer video de los resultados
+    page.wait_for_selector("ytd-video-renderer a#thumbnail")
+    page.click("ytd-video-renderer a#thumbnail")
+    print("Reproduciendo video...")
+    page.wait_for_timeout(5000)
+
+    # Pausar el video presionando la tecla K
+    page.keyboard.press("k")
+    print("Video pausado.")
+    page.wait_for_timeout(2000)
+
+    # Abrir nueva pestaña y entrar a DinoRank
+    email = "seo@agenciadigitalamd.com"
+    password = "SEOMnbvcxz1"
+
+    dinorank_page = browser.new_page()
+    dinorank_page.goto("https://dinorank.com/login/")
+    dinorank_page.wait_for_load_state("networkidle")
+    dinorank_page.wait_for_timeout(3000)
+    dinorank_page.fill("//input[@id='usuario']", email)
+    dinorank_page.fill("//input[@id='password']", password)
+    dinorank_page.click("//button[@id='botonLogin']")
+    dinorank_page.wait_for_load_state("networkidle")
+    dinorank_page.wait_for_timeout(3000)
+    title = dinorank_page.title()
+    print(title.encode(sys.stdout.encoding or "utf-8", errors="replace").decode(sys.stdout.encoding or "utf-8"))
+
+    # Ir a TF*IDF y prominencia
+    dinorank_page.goto("https://dinorank.com/densidad-prominencia/")
+    dinorank_page.wait_for_load_state("networkidle")
+    dinorank_page.wait_for_timeout(3000)
+
+    # Seleccionar el proyecto amerikancash.com
+    dinorank_page.wait_for_selector("//div[@class='dropdown-icon']")
+    dinorank_page.click("//div[@class='dropdown-icon']")
+    dinorank_page.wait_for_timeout(1500)
+    dinorank_page.fill("input#inputSelectorProyecto", "amerikancash")
+    dinorank_page.wait_for_timeout(1500)
+    dinorank_page.click("li:has-text('amerikancash.com')")
+    dinorank_page.wait_for_timeout(2000)
+    print("Proyecto amerikancash.com seleccionado.")
+
+    # Ingresar keyword y URL
+    keyword = "compra y venta de dolares"
+    url_analizar = "https://amerikancash.com/compra-y-venta-de-dolares/"
+    dinorank_page.fill("input#keyword", keyword)
+    dinorank_page.fill("input#url", url_analizar)
+    print("Keyword y URL ingresados.")
+
+    # Click en Analizar y confirmar modal
+    dinorank_page.click("button#buscaKresearch")
+    dinorank_page.wait_for_timeout(2000)
+    dinorank_page.click("button.confirm")
+    print("Analisis iniciado...")
+
+    # Esperar a que carguen las graficas
+    dinorank_page.wait_for_selector("[class*='grafica'], [class*='chart'], canvas, [id*='grafica']", timeout=60000)
+    dinorank_page.wait_for_timeout(3000)
+    print("Graficas de prominencia cargadas.")
